a24de2e9d8a6f5eba66812564217d843b92ce788
Tomycoto <tomchapard42@gmail.com>
2023-11-03 15:38:46 +0900

    firebase db

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/database';    // for realtime database
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCgBEeqpfLUpRcTuF6sA4SZMDR5aBJNsIQ",
+  authDomain: "econnect-db.firebaseapp.com",
+  databaseURL: "https://econnect-db-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "econnect-db",
+  storageBucket: "econnect-db.appspot.com",
+  messagingSenderId: "517009271779",
+  appId: "1:517009271779:web:99ee316bb77de082c0ced6"
+};
+
+firebase.initializeApp(firebaseConfig);
+export const db = firebase.database();
+export default firebase;
